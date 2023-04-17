216045807750feba28514ad3b1507f6b18b8ded5
wlthdud <thdudzi@naver.com>
2023-04-17 18:29:09 +0900

    feat: 페이지 이름 TeamGuu로 변경

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
 //Pages
 import MainPage from './page/MainPage';
