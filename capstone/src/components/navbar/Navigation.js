efd67e38eebecde47076295bea30bebde224bdf2
wlthdud <thdudzi@naver.com>
2023-04-27 15:36:54 +0900

    feat: css파일 module.css로 변경

--- capstone/src/components/navbar/Navigation.js
+++ capstone/src/components/navbar/Navigation.js
@@ -2,70 +2,73 @@ import React from "react";
 import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import {Link} from "react-router-dom";
 
+//style
+import styles from "./Navigation.module.css";
+
 const Navigation = () => {
     return(
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
                 <nav className="navbar navbar-light bg-light">
-                    <div className="container">
+                    <div className={styles.container}>
                         <a className="navbar-brand">
-                        <Link to="/"><img src="teamguuLogo.png" alt="" width="300px" height="110px" /></Link>
+                        <Link to="/"><img src="teamguuLogo.png" alt="로고" width="300px" height="110px" /></Link>
                         </a>
                     </div>
                 </nav>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <NavDropdown title="경기 매칭 신청" id="collasible-nav-dropdown">
+                <Nav className={styles.menubar}>
+                    <NavDropdown title="경기 매칭 신청" id={styles.dropdownNav}>
                         <NavDropdown.Item>
                             <Link to="/page/CreateMatchPage" 
                                 style={{ textDecoration: "none", color:"black" }}>
-                                    매칭 공고 제작
+                                    <div id={styles.subMenuTxt}>매칭 공고 제작</div>
                             </Link>
                         </NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item>
                             <Link to="/page/MatchListPage" 
                                 style={{ textDecoration: "none", color:"black" }}>
-                                    매칭 목록
+                                    <div id={styles.subMenuTxt}>매칭 목록</div>
                             </Link>
                         </NavDropdown.Item>
                     </NavDropdown>
                     <Nav.Link>
                         <Link to="/page/StadiumListPage"
-                            style={{ textDecoration: "none" }}>
-                                경기장 정보
+                            style={{ textDecoration: "none", color:"black" }}>
+                                <div id={styles.fixNav}>경기장 정보</div>
                         </Link>
                     </Nav.Link>
-                    <NavDropdown title="마이페이지" id="collasible-nav-dropdown">
+                    <NavDropdown title="마이페이지" id={styles.dropdownNav}>
                         <NavDropdown.Item>
                             <Link to="/page/MyInfoPage" 
                                 style={{ textDecoration: "none", color:"black" }}>
-                                    내 정보
+                                    <div id={styles.subMenuTxt}>내 정보</div>
                             </Link>
                         </NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item>
                             <Link to="/page/TeamInfoPage"
                                 style={{ textDecoration: "none", color:"black" }}>
-                                    팀 정보
+                                    <div id={styles.subMenuTxt}>팀 정보</div>
                             </Link>
                         </NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item>
                             <Link to="/page/CreateTeamPage"
                                 style={{ textDecoration: "none", color:"black" }}> 
-                                    팀 생성
+                                    <div id={styles.subMenuTxt}>팀 생성</div>
                             </Link>
                         </NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
-                <div className="loginJoin">
+                <div className={styles.loginJoin}>
                     <Link to="/page/LoginPage" style={{ textDecoration: "none", color:"black" }}>
-                        <div className="loginBtn">로그인</div>
+                        <div className={styles.loginBtn}>로그인</div>
                     </Link>
                     <Link to="/page/JoinPage" style={{ textDecoration: "none", color:"black" }}>
-                        <div className="JoinBtn">회원가입</div>
+                        <div className={styles.JoinBtn}>회원가입</div>
                     </Link>
                 </div>
                 </Navbar.Collapse>
