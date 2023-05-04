2e96ad419032c1e11b35c6f85efd13eea96c0103
wlthdud <thdudzi@naver.com>
2023-05-05 06:29:59 +0900

    "feat: 경기장목록 페이지 생성"

--- capstone/src/page/StadiumListPage.js
+++ capstone/src/page/StadiumListPage.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+//style
+import styles from "./StadiumListPage.module.css";
+
 const StadiumListPage = (props) => {
     return(
         <div>
-            <h1>경기장 목록 화면</h1>
+            <div className={styles.topNav}>
+                <ul className={styles.placeNav}>
+                    <li>전체</li>
+                    <li>서울</li>
+                    <li>인천</li>
+                    <li>경기</li>
+                    <li>강원</li>
+                    <li>충청</li>
+                    <li>경상</li>
+                    <li>전라</li>
+                    <li>제주</li>
+                </ul>
+                <div className={styles.searchBar}>
+                    <form>
+                        <input type='text' maxLength='20' className={styles.searchInput} name='search' placeholder='경기장 이름을 입력해주세요.'/>
+                        <input type='submit' value='검색' className={styles.searchSubmit}/>
+                    </form>
+                </div>
+            </div>
         </div>
         
     );
