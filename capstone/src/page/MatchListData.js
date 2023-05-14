f605aed416fd951fe45f44d8b1f4a4240df93ca7
wlthdud <thdudzi@naver.com>
2023-05-14 22:49:30 +0900

    feat: 공고 목록 검색창 구현

---
+++ capstone/src/page/MatchListData.js
@@ -0,0 +1,61 @@
+export const data = [{
+    "name": "명지FC",
+    "score": "35전 20승 15패",
+    "place": "경기",
+    "time": "10AM",
+    "title": "명지FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "서울FC",
+    "score": "28전 20승 8패",
+    "place": "서울",
+    "time": "12PM",
+    "title": "서울FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "경기FC",
+    "score": "5전 1승 4패",
+    "place": "경기",
+    "time": "11AM",
+    "title": "경기FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "인천FC",
+    "score": "12전 8승 4패",
+    "place": "인천",
+    "time": "14PM",
+    "title": "인천FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "목포FC",
+    "score": "29전 19승 10패",
+    "place": "전라",
+    "time": "15PM",
+    "title": "목포FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "강원FC",
+    "score": "54전 32승 22패",
+    "place": "강원",
+    "time": "9AM",
+    "title": "강원FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "용인FC",
+    "score": "9전 7승 2패",
+    "place": "경기",
+    "time": "11AM",
+    "title": "용인FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "부산FC",
+    "score": "2전 2승 0패",
+    "place": "부산",
+    "time": "17PM",
+    "title": "부산FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "제주FC",
+    "score": "16전 4승 12패",
+    "place": "제주",
+    "time": "16PM",
+    "title": "제주FC와 겨룰 팀을 구합니다"
+}, {
+    "name": "충청FC",
+    "score": "20전 9승 11패",
+    "place": "충청",
+    "time": "13PM",
+    "title": "충청FC와 겨룰 팀을 구합니다"
+}] 
--- capstone/src/page/MatchListPage.js
+++ capstone/src/page/MatchListPage.js
@@ -1,125 +1,114 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import CommonTable from "./CommonTable";
-import CommonTableColumn from "./CommonTableColumn";
-import CommonTableRow from "./CommonTableRow";
+import React, {useState} from "react";
+import {data} from "./MatchListData.js"
+import TextField from "@mui/material/TextField";
 
 //styles
 import styles from "./MatchListPage.module.css";
 
-const MatchListPage = props => {
-    return (
-        <>
-          <div className={styles.searchBar}>
-            <div className={styles.hopelocal}>
-                <div className={styles.hopelocalTxt}>
-                  지역
-                </div>
-                <div className={styles.hopelocalSelect}>
-                  <select>
-                    <option>전체</option>
-                    <option>서울</option>
-                    <option>인천</option>
-                    <option>경기</option>
-                    <option>강원</option>
-                    <option>충청</option>
-                    <option>경상</option>
-                    <option>전라</option>
-                    <option>제주</option>
-                  </select>
-                </div>
-            </div>
-            <div className={styles.hopetime}>
-                <div className={styles.hopetimeTxt}>
-                  시작시간
-                </div>
-                <div className={styles.hopetimeSelect}>
-                  <select>
-                    <option>전체</option>
-                    <option>24:00</option>
-                    <option>01:00</option>
-                    <option>02:00</option>
-                    <option>03:00</option>
-                    <option>04:00</option>
-                    <option>05:00</option>
-                    <option>06:00</option>
-                    <option>07:00</option>
-                    <option>08:00</option>
-                    <option>09:00</option>
-                    <option>10:00</option>
-                    <option>11:00</option>
-                    <option>12:00(낮 12시)</option>
-                    <option>13:00</option>
-                    <option>14:00</option>
-                    <option>15:00</option>
-                    <option>16:00</option>
-                    <option>17:00</option>
-                    <option>18:00</option>
-                    <option>19:00</option>
-                    <option>20:00</option>
-                    <option>21:00</option>
-                    <option>22:00</option>
-                    <option>23:00</option>
-                  </select>
-                </div>
-            </div>
-            <div className={styles.search}>
-                <form>
-                    <input type='text' maxLength='20' className={styles.searchInput} name='search' placeholder='검색어를 입력해주세요.'/>
-                    <input type='submit' value='검색' className={styles.searchSubmit}/>
-                </form>
-            </div>
+const MatchListPage = (props) => {
 
-          </div>
-          <hr/>
-          <CommonTable headersName={['팀이름', '프로필', '팀전적', '희망지역','희망시간','공고제목']}>
+  const [search, setSearch] = useState('');
+
+  return ( 
+    <>
+      <div className={styles.searchBar}>
+        <div className={styles.hopelocal}>
+            <div className={styles.hopelocalTxt}>
+              지역
+            </div>
+            <div className={styles.hopelocalSelect}>
+              <select>
+                <option>전체</option>
+                <option>서울</option>
+                <option>인천</option>
+                <option>경기</option>
+                <option>강원</option>
+                <option>충청</option>
+                <option>경상</option>
+                <option>전라</option>
+                <option>제주</option>
+              </select>
+            </div>
+        </div>
+        <div className={styles.hopetime}>
+            <div className={styles.hopetimeTxt}>
+              시작시간
+            </div>
+            <div className={styles.hopetimeSelect}>
+              <select>
+                <option>전체</option>
+                <option>24:00</option>
+                <option>01:00</option>
+                <option>02:00</option>
+                <option>03:00</option>
+                <option>04:00</option>
+                <option>05:00</option>
+                <option>06:00</option>
+                <option>07:00</option>
+                <option>08:00</option>
+                <option>09:00</option>
+                <option>10:00</option>
+                <option>11:00</option>
+                <option>12:00(낮 12시)</option>
+                <option>13:00</option>
+                <option>14:00</option>
+                <option>15:00</option>
+                <option>16:00</option>
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+                <option>21:00</option>
+                <option>22:00</option>
+                <option>23:00</option>
+              </select>
+            </div>
+        </div>
+        <div className={styles.search}>
+            <TextField
+            id="outlined-basic"
+            onChange={(e)=>setSearch(e.target.value)}
+            variant="outlined"
+            fullWidth
+            label="팀명을 입력하세요."
+            />
+        </div>
+      </div>
+      <hr/>
+      <div className={styles.TableWrap}>
+      <table className={styles.matchListTable}>
+            <thead className={styles.tableHead}>
+              <tr>
+                <th>팀명</th>
+                <th>전적</th>
+                <th>경기지역</th>
+                <th>시작시간</th>
+                <th>제목</th>
+              </tr>
+            </thead>
+            <tbody className={styles.tableBody}>
+              {data.filter((item)=>{
+                return search.toLowerCase() === '' 
+                  ? item 
+                  : item.name.toLowerCase().includes(search);
+              }).map((item) => (
+                <tr key={item.name}>
+                    <td>{item.name}</td>
+                    <td>{item.score}</td>
+                    <td>{item.place}</td>
+                    <td>{item.time}</td>
+                    <td>{item.title}</td>
+                </tr>
+              ))}
+                
+            </tbody>
+        </table>
+      </div>
+      
             
-            <CommonTableRow>
-              <Link to="/page/MatchInfoPage" style={{textDecoration: "none"}}>
-              <CommonTableColumn>명지FC</CommonTableColumn>
-              </Link>
-              <CommonTableColumn>이미지</CommonTableColumn>
-              <CommonTableColumn>35전20승15패</CommonTableColumn>
-              <CommonTableColumn>서울</CommonTableColumn>
-              <CommonTableColumn>18pm~21pm</CommonTableColumn>
-              <CommonTableColumn>명지FC와 겨룰팀을 구합니다.</CommonTableColumn>
-            </CommonTableRow>
             
-            <CommonTableRow>
-              <CommonTableColumn>서울FC</CommonTableColumn>
-              <CommonTableColumn>이미지</CommonTableColumn>
-              <CommonTableColumn>35전20승15패</CommonTableColumn>
-              <CommonTableColumn>서울</CommonTableColumn>
-              <CommonTableColumn>18pm~21pm</CommonTableColumn>
-              <CommonTableColumn>서울FC와 겨룰팀을 구합니다.</CommonTableColumn>
-            </CommonTableRow>
-            <CommonTableRow>
-              <CommonTableColumn>경기FC</CommonTableColumn>
-              <CommonTableColumn>이미지</CommonTableColumn>
-              <CommonTableColumn>35전20승15패</CommonTableColumn>
-              <CommonTableColumn>용인</CommonTableColumn>
-              <CommonTableColumn>18pm~21pm</CommonTableColumn>
-              <CommonTableColumn>경기FC와 겨룰팀을 구합니다.</CommonTableColumn>
-            </CommonTableRow>
-            <CommonTableRow>
-              <CommonTableColumn>목포FC</CommonTableColumn>
-              <CommonTableColumn>이미지</CommonTableColumn>
-              <CommonTableColumn>35전20승15패</CommonTableColumn>
-              <CommonTableColumn>목포</CommonTableColumn>
-              <CommonTableColumn>18pm~21pm</CommonTableColumn>
-              <CommonTableColumn>목포FC와 겨룰팀을 구합니다.</CommonTableColumn>
-            </CommonTableRow>
-            <CommonTableRow>
-              <CommonTableColumn>용인FC</CommonTableColumn>
-              <CommonTableColumn>이미지</CommonTableColumn>
-              <CommonTableColumn>35전20승15패</CommonTableColumn>
-              <CommonTableColumn>인천</CommonTableColumn>
-              <CommonTableColumn>18pm~21pm</CommonTableColumn>
-              <CommonTableColumn>용인FC와 겨룰팀을 구합니다.</CommonTableColumn>
-            </CommonTableRow>
-          </CommonTable>
-        </>
-      )
-  }
+    </>
+  );
+}
 
 export default MatchListPage;
