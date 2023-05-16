9e88f8102dc4414c38eb2092610fd3e47c0810ad
[Github <[Github>
2023-05-16 16:22:02 +0900

    docs: 매칭공고제작 기능 구현완료+팀선택가능

--- capstone/src/page/CreateMatchPage.js
+++ capstone/src/page/CreateMatchPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import DatePicker from "react-datepicker";
 import { ko } from 'date-fns/esm/locale';
 import axios from 'axios';
@@ -7,30 +7,38 @@ import styles from "./CreateMatchPage.module.css";
 import "react-datepicker/dist/react-datepicker.css";
 
 const CreateMatchPage = (props) => {
+    const [selectedTeamOption, setSelectedTeamOption] = useState('');
     const [selectedOption, setSelectedOption] = useState('전체');
     const [startDate, setStartDate] = useState(new Date());
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
 
-    function handleOptionChange(event) {
-        console.log(event.target.value);
-      setSelectedOption(event.target.value);
-    }
+    const [teamInfo, setTeamInfo] = useState([]); // 팀 ID 저장용 상태 변수
+    const [teamId, setTeamId] = useState(0); //선택된 팀의 id를 저장할 변수
+  
+    useEffect(() => {
+      console.log(`선택된 팀의 아이디: ${teamId}`);
+    }, [teamId]);
 
-    const handleDateChange = (date) => {
-        setStartDate(date);
-        console.log(startDate.toISOString().split("T")[0]); // 선택한 날짜 출력
-      };
 
-    const handleTitleChange = (e) => {
-       setTitle(e.target.value);
-      console.log(title);
-   };
-
-    const handleContentChange = (e) => {
-      setContent(e.target.value);
-      console.log(content);
-    };
+    useEffect(() => {
+      axios
+        .get("http://www.teamguu.p-e.kr/api/teams/simple", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+        .then((response) => {
+          console.log("팀 정보 조회성공");
+          setTeamInfo(response.data.result);
+          console.log(response.data.result);
+          
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log(`엑세스토큰: ${localStorage.getItem("accessToken")}`);
+        });
+    }, []);
 
   const handleSubmit = (e) => {
  
@@ -46,7 +54,7 @@ const CreateMatchPage = (props) => {
         content: content
       };
 
-    axios.post('http://www.teamguu.p-e.kr/api/matches?teamId=1', requestBody, {
+    axios.post(`http://www.teamguu.p-e.kr/api/matches?teamId=${teamId}`, requestBody, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
         },
@@ -54,15 +62,48 @@ const CreateMatchPage = (props) => {
     .then((response) => {
         console.log(response);
         alert('매칭 공고 등록이 완료되었습니다.');
-        
+        console.log(`보낼때의 팀 id: ${teamId}`);
       })
       .catch((error) => {
         console.error(error);
         alert('매칭 공고 등록에 실패했습니다.');
-        
+        console.log(`보낼때의 팀 id: ${teamId}`);
       }); 
   };
 
+
+
+
+  function handleOptionChange(event) {
+    console.log(event.target.value);
+  setSelectedOption(event.target.value);
+}
+
+const handleDateChange = (date) => {
+    setStartDate(date);
+    console.log(startDate.toISOString().split("T")[0]); // 선택한 날짜 출력
+  };
+
+const handleTitleChange = (e) => {
+   setTitle(e.target.value);
+  console.log(title);
+};
+
+const handleContentChange = (e) => {
+  setContent(e.target.value);
+  console.log(content);
+};
+
+
+const handleTeamChange = (event) => {
+  setSelectedTeamOption(event.target.value); // 선택된 팀의 이름 저장
+  const selectedTeam = teamInfo.find((team) => team.name === event.target.value); // 선택된 팀 정보 찾기
+  if (selectedTeam) {
+    console.log(selectedTeam.id); // 선택된 팀의 ID 출력
+    setTeamId(selectedTeam.id);
+  }
+};
+  
     return(
         <div>
             <div className={styles.topWrap}>
@@ -70,6 +111,17 @@ const CreateMatchPage = (props) => {
                 <div className={styles.registerBtn } onClick={handleSubmit}>등록</div>
             </div>
             <div className={styles.bottomWrap}>
+                <div className={styles.place}>
+                    <div className={styles.selectTitle}>팀 선택</div>
+                    <div className={styles.inputWrap}>
+                      <select value={selectedTeamOption} onChange={handleTeamChange}>
+                        <option value="">전체</option>
+                        {teamInfo.map((team) => (
+                          <option key={team.id} value={team.name}>{team.name}</option>
+                        ))}
+                      </select>
+                    </div>
+                </div>
                 <div className={styles.place}>
                     <div className={styles.selectTitle}>
                         장소    
